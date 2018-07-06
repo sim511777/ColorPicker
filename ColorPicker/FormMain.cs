f29d4c7be18d7040dc9fe1589955c062f7ee7cf7
sim511777 <sim51177@gmail.com>
2018-07-07 04:13:47 +0900

    finish

--- ColorPicker/FormMain.cs
+++ ColorPicker/FormMain.cs
@@ -16,43 +16,47 @@ namespace ColorPicker {
          InitializeComponent();
       }
 
-      private void pbxCapture_MouseMove(object sender, MouseEventArgs e) {
+      private void pbxCap_MouseMove(object sender, MouseEventArgs e) {
          if (Control.MouseButtons != MouseButtons.Left)
             return;
 
-         Color pickColor;
-         
-         int zoom = (int)this.numZoom.Value;
-         
-         using (var bmp = new Bitmap(this.pbxCapture.Size.Width/zoom, this.pbxCapture.Size.Height/zoom))
-         using (var bmpGfx = Graphics.FromImage(bmp)) {
-            var scrPt = this.pbxCapture.PointToScreen(e.Location)-new Size(bmp.Width/2, bmp.Height/2);
-            
-            bmpGfx.CopyFromScreen(scrPt, Point.Empty, bmp.Size);  // capture desktop
+         int zoom = 4;
+         var scrPt = this.pbxCap.PointToScreen(e.Location)-new Size(this.pbxCap.Width/zoom/2, this.pbxCap.Height/zoom/2);
+         var bmp = new Bitmap(this.pbxCap.Width/zoom, this.pbxCap.Height/zoom);
 
-            pickColor = bmp.GetPixel(bmp.Width/2, bmp.Height/2);  // get pick color
-            
-            using (Graphics gPbx = this.pbxCapture.CreateGraphics()) {
-               gPbx.InterpolationMode = InterpolationMode.NearestNeighbor;
-               gPbx.DrawImage(bmp, this.pbxCapture.ClientRectangle); // draw capture box
-               
-               // draw crosshair
-               Pen pen = new Pen(Color.Yellow);
-               pen.DashStyle = DashStyle.Dot;
-               gPbx.DrawLine(pen, this.pbxCapture.Width/2, 0, this.pbxCapture.Width/2, this.pbxCapture.Height);
-               gPbx.DrawLine(pen, 0, this.pbxCapture.Height/2, this.pbxCapture.Width, this.pbxCapture.Height/2);
-               pen.Dispose();
-            } 
-         }
+         // capture
+         var gfxBmp = Graphics.FromImage(bmp);
+         gfxBmp.CopyFromScreen(scrPt, Point.Empty, bmp.Size);
+         gfxBmp.Dispose();
 
-         // fill color box
-         using (Brush br = new SolidBrush(pickColor))
-         using (Graphics gColor = this.pbxColor.CreateGraphics()) {
-            gColor.FillRectangle(br, this.pbxColor.ClientRectangle);
-         }
+         // get pick color
+         Color pickColor = bmp.GetPixel(bmp.Width/2, bmp.Height/2);
+
+         // draw crosshair
+         bmp.SetPixel(bmp.Width/2-2, bmp.Height/2, Color.Green);
+         bmp.SetPixel(bmp.Width/2, bmp.Height/2-2, Color.Green);
+         bmp.SetPixel(bmp.Width/2+2, bmp.Height/2, Color.Green);
+         bmp.SetPixel(bmp.Width/2, bmp.Height/2+2, Color.Green);
+
+         // draw capture
+         var gfxCap = this.pbxCap.CreateGraphics();
+         gfxCap.InterpolationMode = InterpolationMode.NearestNeighbor;
+         gfxCap.FillRectangle(Brushes.Black, this.pbxCap.DisplayRectangle);
+         gfxCap.DrawImage(bmp, this.pbxCap.ClientRectangle);
+         gfxCap.DrawRectangle(Pens.Black, this.pbxCap.ClientRectangle);
+         
+         // draw color rectangle
+         var brColor = new SolidBrush(pickColor);
+         var rectColor = new Rectangle(0, 0, this.pbxCap.Width/3, this.pbxCap.Height/3);
+         gfxCap.FillRectangle(brColor, rectColor);
+         gfxCap.DrawRectangle(Pens.Black, rectColor);
+         brColor.Dispose();
+
+         gfxCap.Dispose();
+         bmp.Dispose();
 
          // display color value
-         this.tbxRgb.Text = $"{pickColor.R},{pickColor.G},{pickColor.B}";
+         this.tbxRgb.Text = $"({pickColor.R}, {pickColor.G}, {pickColor.B})";
          this.tbxHex.Text = ColorTranslator.ToHtml(pickColor);
          this.tbxRgb.Refresh();
          this.tbxHex.Refresh();
