1048c58c95f8629f3dbf6a612cfb9e1db8142ccb
sim511777 <sim51177@gmail.com>
2018-07-07 04:13:47 +0900

    finish

--- ColorPicker/FormMain.Designer.cs
+++ ColorPicker/FormMain.Designer.cs
@@ -23,33 +23,28 @@
       /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
       /// </summary>
       private void InitializeComponent() {
-         this.pbxCapture = new System.Windows.Forms.PictureBox();
+         this.pbxCap = new System.Windows.Forms.PictureBox();
          this.tbxHex = new System.Windows.Forms.TextBox();
          this.tbxRgb = new System.Windows.Forms.TextBox();
-         this.numZoom = new System.Windows.Forms.NumericUpDown();
-         this.label3 = new System.Windows.Forms.Label();
-         this.pbxColor = new System.Windows.Forms.PictureBox();
          this.label2 = new System.Windows.Forms.Label();
          this.label1 = new System.Windows.Forms.Label();
-         ((System.ComponentModel.ISupportInitialize)(this.pbxCapture)).BeginInit();
-         ((System.ComponentModel.ISupportInitialize)(this.numZoom)).BeginInit();
-         ((System.ComponentModel.ISupportInitialize)(this.pbxColor)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.pbxCap)).BeginInit();
          this.SuspendLayout();
          // 
-         // pbxCapture
+         // pbxCap
          // 
-         this.pbxCapture.BackColor = System.Drawing.Color.Black;
-         this.pbxCapture.Location = new System.Drawing.Point(12, 12);
-         this.pbxCapture.Name = "pbxCapture";
-         this.pbxCapture.Size = new System.Drawing.Size(128, 128);
-         this.pbxCapture.TabIndex = 0;
-         this.pbxCapture.TabStop = false;
-         this.pbxCapture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxCapture_MouseMove);
+         this.pbxCap.BackColor = System.Drawing.Color.Black;
+         this.pbxCap.Location = new System.Drawing.Point(12, 12);
+         this.pbxCap.Name = "pbxCap";
+         this.pbxCap.Size = new System.Drawing.Size(256, 256);
+         this.pbxCap.TabIndex = 0;
+         this.pbxCap.TabStop = false;
+         this.pbxCap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbxCap_MouseMove);
          // 
          // tbxHex
          // 
          this.tbxHex.BackColor = System.Drawing.SystemColors.Window;
-         this.tbxHex.Location = new System.Drawing.Point(176, 91);
+         this.tbxHex.Location = new System.Drawing.Point(198, 274);
          this.tbxHex.Name = "tbxHex";
          this.tbxHex.ReadOnly = true;
          this.tbxHex.Size = new System.Drawing.Size(70, 21);
@@ -58,65 +53,25 @@
          // tbxRgb
          // 
          this.tbxRgb.BackColor = System.Drawing.SystemColors.Window;
-         this.tbxRgb.Location = new System.Drawing.Point(176, 66);
+         this.tbxRgb.Location = new System.Drawing.Point(53, 274);
          this.tbxRgb.Name = "tbxRgb";
          this.tbxRgb.ReadOnly = true;
-         this.tbxRgb.Size = new System.Drawing.Size(70, 21);
+         this.tbxRgb.Size = new System.Drawing.Size(90, 21);
          this.tbxRgb.TabIndex = 2;
-         // 
-         // numZoom
-         // 
-         this.numZoom.Location = new System.Drawing.Point(192, 119);
-         this.numZoom.Maximum = new decimal(new int[] {
-            128,
-            0,
-            0,
-            0});
-         this.numZoom.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-         this.numZoom.Name = "numZoom";
-         this.numZoom.Size = new System.Drawing.Size(54, 21);
-         this.numZoom.TabIndex = 3;
-         this.numZoom.Value = new decimal(new int[] {
-            4,
-            0,
-            0,
-            0});
-         // 
-         // label3
-         // 
-         this.label3.AutoSize = true;
-         this.label3.Location = new System.Drawing.Point(144, 121);
-         this.label3.Name = "label3";
-         this.label3.Size = new System.Drawing.Size(42, 12);
-         this.label3.TabIndex = 1;
-         this.label3.Text = "Zoom:";
-         // 
-         // pbxColor
-         // 
-         this.pbxColor.BackColor = System.Drawing.Color.Black;
-         this.pbxColor.Location = new System.Drawing.Point(146, 12);
-         this.pbxColor.Name = "pbxColor";
-         this.pbxColor.Size = new System.Drawing.Size(100, 48);
-         this.pbxColor.TabIndex = 0;
-         this.pbxColor.TabStop = false;
          // 
          // label2
          // 
          this.label2.AutoSize = true;
-         this.label2.Location = new System.Drawing.Point(144, 94);
+         this.label2.Location = new System.Drawing.Point(149, 277);
          this.label2.Name = "label2";
-         this.label2.Size = new System.Drawing.Size(31, 12);
+         this.label2.Size = new System.Drawing.Size(43, 12);
          this.label2.TabIndex = 1;
-         this.label2.Text = "Hex:";
+         this.label2.Text = "HTML:";
          // 
          // label1
          // 
          this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(144, 69);
+         this.label1.Location = new System.Drawing.Point(13, 277);
          this.label1.Name = "label1";
          this.label1.Size = new System.Drawing.Size(34, 12);
          this.label1.TabIndex = 1;
@@ -126,23 +81,18 @@
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(257, 152);
-         this.Controls.Add(this.numZoom);
+         this.ClientSize = new System.Drawing.Size(280, 303);
          this.Controls.Add(this.tbxRgb);
-         this.Controls.Add(this.label3);
          this.Controls.Add(this.label2);
          this.Controls.Add(this.tbxHex);
          this.Controls.Add(this.label1);
-         this.Controls.Add(this.pbxColor);
-         this.Controls.Add(this.pbxCapture);
+         this.Controls.Add(this.pbxCap);
          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
          this.MaximizeBox = false;
          this.MinimizeBox = false;
          this.Name = "FormMain";
          this.Text = "Color Picker - Drag big box!!!";
-         ((System.ComponentModel.ISupportInitialize)(this.pbxCapture)).EndInit();
-         ((System.ComponentModel.ISupportInitialize)(this.numZoom)).EndInit();
-         ((System.ComponentModel.ISupportInitialize)(this.pbxColor)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.pbxCap)).EndInit();
          this.ResumeLayout(false);
          this.PerformLayout();
 
@@ -150,12 +100,9 @@
 
       #endregion
 
-      private System.Windows.Forms.PictureBox pbxCapture;
+      private System.Windows.Forms.PictureBox pbxCap;
       private System.Windows.Forms.TextBox tbxHex;
       private System.Windows.Forms.TextBox tbxRgb;
-      private System.Windows.Forms.NumericUpDown numZoom;
-      private System.Windows.Forms.Label label3;
-      private System.Windows.Forms.PictureBox pbxColor;
       private System.Windows.Forms.Label label2;
       private System.Windows.Forms.Label label1;
    }
