3c3303cadc1a4f7b0ad1ef46d77a9d95ea2803e6
sim511777 <sim51177@gmail.com>
2020-02-25 23:30:50 +0900

    formatter

--- ColorPicker/FormMain.cs
+++ ColorPicker/FormMain.cs
@@ -7,84 +7,84 @@ using System.Windows.Forms;
 // change to WmPaint
 
 namespace ColorPicker {
-   public partial class FormMain : Form {
-      private int zoom;
-      private Bitmap bmp;
-      Color colPick;// = Color.Black;
+    public partial class FormMain : Form {
+        private int zoom;
+        private Bitmap bmp;
+        Color colPick;// = Color.Black;
 
-      public FormMain() {
-         InitializeComponent();
-         this.zoom = 4;
-         this.bmp = new Bitmap(this.pbxCap.Width / zoom, this.pbxCap.Height / zoom);
-      }
+        public FormMain() {
+            InitializeComponent();
+            this.zoom = 4;
+            this.bmp = new Bitmap(this.pbxCap.Width / zoom, this.pbxCap.Height / zoom);
+        }
 
-      private void CaptureScreen(Point pt) {
-         var ptCap = pt - new Size(this.bmp.Width / 2, this.bmp.Height / 2);
-         var g = Graphics.FromImage(this.bmp);
-         g.FillRectangle(Brushes.Gray, 0, 0, this.bmp.Width, this.bmp.Height);
-         g.CopyFromScreen(ptCap, Point.Empty, this.bmp.Size);
-         g.Dispose();
-      }
+        private void CaptureScreen(Point pt) {
+            var ptCap = pt - new Size(this.bmp.Width / 2, this.bmp.Height / 2);
+            var g = Graphics.FromImage(this.bmp);
+            g.FillRectangle(Brushes.Gray, 0, 0, this.bmp.Width, this.bmp.Height);
+            g.CopyFromScreen(ptCap, Point.Empty, this.bmp.Size);
+            g.Dispose();
+        }
 
-      private void GetColor() {
-         colPick = bmp.GetPixel(bmp.Width / 2, bmp.Height / 2);
-      }
+        private void GetColor() {
+            colPick = bmp.GetPixel(bmp.Width / 2, bmp.Height / 2);
+        }
 
-      private void DrawCrosshairToBmp() {
-         int size = 2;
-         Point[] pt4 = {
+        private void DrawCrosshairToBmp() {
+            int size = 2;
+            Point[] pt4 = {
             new Point(bmp.Width / 2 - size, bmp.Height / 2),
             new Point(bmp.Width / 2, bmp.Height / 2 - size),
             new Point(bmp.Width / 2 + size, bmp.Height / 2),
             new Point(bmp.Width / 2, bmp.Height / 2 + size)
          };
-         foreach (var pt in pt4) {
-            Color colBg = bmp.GetPixel(pt.X, pt.Y);
-            Color col = Color.Fuchsia;
-            float f = 0.7f;
-            int r = (int)(col.R * f + colBg.R * (1-f));
-            int g = (int)(col.G * f + colBg.G * (1-f));
-            int b = (int)(col.B * f + colBg.B * (1-f));
-            Color colNew = Color.FromArgb(r, g, b);
-            bmp.SetPixel(pt.X, pt.Y, colNew);
-         }
-      }
+            foreach (var pt in pt4) {
+                Color colBg = bmp.GetPixel(pt.X, pt.Y);
+                Color col = Color.Fuchsia;
+                float f = 0.7f;
+                int r = (int)(col.R * f + colBg.R * (1 - f));
+                int g = (int)(col.G * f + colBg.G * (1 - f));
+                int b = (int)(col.B * f + colBg.B * (1 - f));
+                Color colNew = Color.FromArgb(r, g, b);
+                bmp.SetPixel(pt.X, pt.Y, colNew);
+            }
+        }
 
-      private void DrawBmpToPictureBox(Graphics g) {
-         // draw capture rectangle
-         Rectangle rectCap = this.pbxCap.DisplayRectangle;
-         g.InterpolationMode = InterpolationMode.NearestNeighbor;
-         g.DrawImage(bmp, rectCap);
-         g.DrawRectangle(Pens.Black, rectCap);
+        private void DrawBmpToPictureBox(Graphics g) {
+            // draw capture rectangle
+            Rectangle rectCap = this.pbxCap.DisplayRectangle;
+            g.InterpolationMode = InterpolationMode.NearestNeighbor;
+            g.DrawImage(bmp, rectCap);
+            g.DrawRectangle(Pens.Black, rectCap);
 
-         // draw color rectangle
-         var rectColor = new Rectangle(0, 0, 90, 90);
-         var br = new SolidBrush(colPick);
-         g.FillRectangle(br, rectColor);
-         br.Dispose();
-         g.DrawRectangle(Pens.Black, rectColor);
-      }
+            // draw color rectangle
+            var rectColor = new Rectangle(0, 0, 90, 90);
+            var br = new SolidBrush(colPick);
+            g.FillRectangle(br, rectColor);
+            br.Dispose();
+            g.DrawRectangle(Pens.Black, rectColor);
+        }
 
-      private void DisplayColorValue() {
-         this.tbxRgb.Text = $"({this.colPick.R}, {this.colPick.G}, {this.colPick.B})";
-         this.tbxHtml.Text = ColorTranslator.ToHtml(this.colPick);
-         this.tbxRgb.Refresh();
-         this.tbxHtml.Refresh();
-      }
+        private void DisplayColorValue() {
+            this.tbxRgb.Text = $"({this.colPick.R}, {this.colPick.G}, {this.colPick.B})";
+            this.tbxHtml.Text = ColorTranslator.ToHtml(this.colPick);
+            this.tbxRgb.Refresh();
+            this.tbxHtml.Refresh();
+        }
 
-      private void pbxCap_MouseMove(object sender, MouseEventArgs e) {
-         if (Control.MouseButtons != MouseButtons.Left)
-            return;
+        private void pbxCap_MouseMove(object sender, MouseEventArgs e) {
+            if (Control.MouseButtons != MouseButtons.Left)
+                return;
 
-         this.CaptureScreen(Cursor.Position);
-         this.GetColor();
-         this.pbxCap.Refresh();
-         this.DisplayColorValue();
-      }
+            this.CaptureScreen(Cursor.Position);
+            this.GetColor();
+            this.pbxCap.Refresh();
+            this.DisplayColorValue();
+        }
 
-      private void pbxCap_Paint(object sender, PaintEventArgs e) {
-         this.DrawCrosshairToBmp();
-         this.DrawBmpToPictureBox(e.Graphics);
-      }
-   }
+        private void pbxCap_Paint(object sender, PaintEventArgs e) {
+            this.DrawCrosshairToBmp();
+            this.DrawBmpToPictureBox(e.Graphics);
+        }
+    }
 }
